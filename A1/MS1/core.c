a0e9f8f8f4ce935eae5de04d697871a838039060
Nathan <mhk.nathan@gmail.com>
2023-11-17 15:51:07 -0500

    Implemented inputCString()
    1. Accept '\s' as a valid char
    2. Discard the first leading '\n' only
    3. Assumes input is not always valid (e.g. input might contain digits or symbols)
    4. Trailing '\n' will be discarded

--- A1/MS1/core.c
+++ A1/MS1/core.c
@@ -115,8 +115,118 @@ char inputCharOption(const char validChars[]) {
     return value;
 }
 
-void inputCString(char* const str, int min, int max) {
-    // TODO
+void inputCString(char* const str, const int min, const int max) {
+    const char* const validChars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM ";
+
+    char* i = str;
+    const char* validChar = validChars;
+    int len = 0;
+    int repeat = 0, reset = 0;
+    int ueqLen = 0;
+    char alphbt = '\0';
+
+    do {
+        repeat = 1;
+        scanf("%c", &alphbt);
+
+        // Discard leading '\n'
+        // !(i == str && alphbt == '\n')
+        if (i != str || alphbt != '\n') {
+            /**
+             * Too short:
+             *      '\n' && len < min
+             * Too long:
+             *      '\n' && len > max
+             *      !'\n' && len == max
+             *      !'\n' && len > max
+             * Not equal:
+             *      Any of above && min == max 
+            */
+            repeat = alphbt != '\n';
+            reset = 0;
+            ueqLen = 0;
+
+            if (alphbt == '\n' && len < min) {
+                if (min == max) {
+                    // Not equal
+                    ueqLen = 1;
+                } else {
+                    // Too short
+                    reset = 1;
+                    printf("ERROR: String length must be between %d and %d chars: ", min, max);
+                }
+            } else if ((alphbt == '\n' && len > max) || (alphbt != '\n' && len >= max)) {
+                if (min == max) {
+                    // Not equal
+                    ueqLen = 1;
+                } else {
+                    // Too long
+                    reset = 1;
+                    printf("ERROR: String length must be no more than %d chars: ", max);
+                }
+            }
+
+            if (ueqLen) {
+                reset = 1;
+                printf("ERROR: String length must be exactly %d chars: ", min);
+            }
+
+            // Assume always valid?
+            if (!reset && repeat) {
+                // Check if input char is valid
+                reset = 1;
+                for (validChar = validChars; *validChar != '\0' && reset; ++validChar) {
+                    reset = alphbt != *validChar;
+                }
+                if (reset) {
+                    printf("ERROR: Invalid character\n");
+                }
+            }
+
+            if (reset) {
+                for (i = str; *i != '\0'; ++i) {
+                    *i = '\0';
+                }
+                i = str;
+                len = 0;
+                if (repeat) {
+                    clearInputBuffer();
+                }
+            } else if (repeat) {
+                // Prevent putting '\n' into the string
+                *i = alphbt;
+                ++i;
+                ++len;
+            }
+        }
+    } while (reset || repeat);
+
+    // do {
+    //     scanf(" %[^\n]", str);
+
+    //     // Expand this for loop to handle the strLen
+    //     // Might get SegFault if input is longer than the length of str
+    //     for (i = str; *i != '\0'; ++i, ++len);
+
+    //     if (min == max && len != min) {
+    //         repeat = 1;
+    //         printf("ERROR: String length must be exactly %d chars: ", min);
+    //     } else if (len < min) {
+    //         repeat = 1;
+    //         printf("ERROR: String length must be between %d and %d chars: ", min, max);
+    //     } else if (len > max) {
+    //         repeat = 1;
+    //         printf("ERROR: String length must be no more than %d chars: ", max);
+    //     } else {
+    //         repeat = 0;
+    //     }
+
+    //     if (repeat) {
+    //         clearInputBuffer();
+    //     }
+
+    // } while (repeat);
+
 }
 
 void displayFormattedPhone(const char* const str) {
